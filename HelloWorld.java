0e6e8c063468b1ccd4e0aafefa447c11215a39da
raj711x <57318073+raj711x@users.noreply.github.com>
2019-11-24 10:50:11 +0000

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+
+/**
+ * Hello World - to start writting code in Java
+ * 
+ * @author (R.Patel) 
+ * @version (V1.0 - 23rd Nov 2019)
+ */
+class HelloWorld {
+    public static void main(String[] args)
+    {
+        int y=15;
+        for(int x = 1; x<=y; x++) {
+        // just going to print out Hello world
+        System.out.println("HellowWorld!");
+        } 
+    System.out.println("End of program!");
+   
+    }
+}
